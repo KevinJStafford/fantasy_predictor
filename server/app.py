6674de5f331d8733c59075f98b8817e24f656782
Kevin <staffordkevinjames@gmail.com>
2023-12-08 11:01:04 -0500

    data tables created

--- server/app.py
+++ server/app.py
@@ -3,13 +3,30 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
-from flask_restful import Resource
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+from flask_migrate import Migrate
+import os
 
 # Local imports
 from config import app, db, api
 # Add your model imports
+from models import db, User, Game, Prediction
 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DATABASE = os.environ.get(
+    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+api = Api(app)
 
 # Views go here!
 
--- server/models.py
+++ server/models.py
@@ -1,6 +1,86 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy import MetaData
+from flask_sqlalchemy import SQLAlchemy
 
-from config import db
+from config import db, bcrypt
+
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metadata = MetaData(naming_convention=convention)
+
+db = SQLAlchemy(metadata=metadata)
 
 # Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    serialize_rules=('-password_hash', )
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    _password_hash = db.Column(db.String)
+    game_id = db.Column(db.Integer, db.ForeignKey('games.id'))
+    games = db.relationship('Game', back_populates='game')
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
+
+    # Relationships
+    predictions = db.relationship('Prediction', back_populates='user', cascade='all, delete-orphan')
+    games = association_proxy('predictions', 'game')
+
+    @property
+    def password_hash(self):
+        return self.password_hash
+    
+    @password_hash.setter
+    def password_hash(self, plain_text_password):
+        byte_object = plain_text_password.encode('utf-8')
+        encrypted_password_object = bcrypt.generate_password_hash(byte_object)
+        hashed_password_string = encrypted_password_object.decode('utf-8')
+        self._password_hash = hashed_password_string
+        
+    def authenticate(self, password_string):
+        byte_object = password_string.encode('utf-8')
+        return bcrypt.check_password_hash(self.password_hash, byte_object)
+    
+    def __repr__(self):
+        return f'<User {self.id}: {self.username}>'
+
+class Prediction(db.Model, SerializerMixin):
+    __tablename__ = 'predictions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    game_id = db.Column(db.Integer, db.ForeignKey('games.id'))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
+
+class Game(db.Model, SerializerMixin):
+    __tablename__ = 'games'
+
+    id = db.Column(db.Integer, primary_key=True)
+    game_week_name = db.Column(db.String)
+    game_week = db.Column(db.DateTime)
+    home_team = db.Column(db.String)
+    home_team_score = db.Column(db.Integer)
+    away_team = db.Column(db.String)
+    away_team_score = db.Column(db.Integer)
+    game_result = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
+
+    # Relationships
+    predictions = db.relationship('Prediction', back_populates='game', cascade='all, delete-orphan')
+    users = association_proxy('predictions', 'user')
+
+    def __repr__(self):
+        return f'<Game {self.id}: {self.game_week_name}>'
