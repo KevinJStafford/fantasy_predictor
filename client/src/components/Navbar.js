1982d0e245efc67810d2dcdad97714d4fe6b41e3
Kevin <staffordkevinjames@gmail.com>
2026-01-25 11:21:51 -0500

    Fix Chakra UI error by converting Navbar to Material UI

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,42 +1,33 @@
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
-import { Flex, Spacer, Box, Image } from '@chakra-ui/react'
+import { Box, AppBar, Toolbar } from '@mui/material';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import { yellow } from '@mui/material/colors';
 
 function Navbar() {
-    try {
-        return (
-            <Flex>
-                <Box p='4' bg='red.400'>
-                    <Image 
-                        justify="left" 
+    return (
+        <AppBar position="static" sx={{ backgroundColor: '#fff', boxShadow: 1 }}>
+            <Toolbar sx={{ justifyContent: 'space-between', px: 2 }}>
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <img 
                         className="nav_logo" 
-                        objectFit='cover' 
                         src="https://dl.dropboxusercontent.com/scl/fi/jz578vjpoujkt4o7n0o65/header_navbar_2.png?rlkey=e85yc3b29u3x8nbk8yxkmiw2t&dl=0" 
                         alt='Fantasy Predictor text logo'
+                        style={{ maxHeight: '60px', objectFit: 'contain' }}
                         onError={(e) => {
                             console.error('Navbar image failed to load');
                             e.target.style.display = 'none';
                         }}
                     />
                 </Box>
-                <Spacer />
-                <Box p='4' bg='green.400'>
-                    <Avatar sx={{bgcolor: yellow[500] }} variant='rounded'>
+                <Box>
+                    <Avatar sx={{ bgcolor: yellow[500] }} variant='rounded'>
                         <AccountBoxIcon />
                     </Avatar>
                 </Box>
-            </Flex>
-        );
-    } catch (error) {
-        console.error('Navbar render error:', error);
-        return (
-            <div style={{ padding: '10px', backgroundColor: '#f0f0f0' }}>
-                <h3>Fantasy Predictor</h3>
-            </div>
-        );
-    }
+            </Toolbar>
+        </AppBar>
+    );
 }
 
 export default Navbar;
--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,13 @@ import App from "./components/App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom/";
 import ReactDOM from "react-dom"
-import { ChakraProvider } from '@chakra-ui/react'
 import ErrorBoundary from "./components/ErrorBoundary"
 
 ReactDOM.render(
     <ErrorBoundary>
-      <ChakraProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ChakraProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ErrorBoundary>,
     document.getElementById("root")
   );
