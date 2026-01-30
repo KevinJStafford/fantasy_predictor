77e767cb68b83cdf81638811639892deef18f28d
Kevin <staffordkevinjames@gmail.com>
2026-01-30 10:21:33 -0500

    change: navbar image & favicon

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,6 +3,7 @@ import Avatar from '@mui/material/Avatar';
 import { Box, AppBar, Toolbar } from '@mui/material';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import { yellow } from '@mui/material/colors';
+import typefaceLogoNew from '../../images/typeface_logo_new.png';
 
 function Navbar() {
     return (
@@ -11,8 +12,8 @@ function Navbar() {
                 <Box sx={{ display: 'flex', alignItems: 'center' }}>
                     <img 
                         className="nav_logo" 
-                        src="https://dl.dropboxusercontent.com/scl/fi/jz578vjpoujkt4o7n0o65/header_navbar_2.png?rlkey=e85yc3b29u3x8nbk8yxkmiw2t&dl=0" 
-                        alt='Fantasy Predictor text logo'
+                        src={typefaceLogoNew}
+                        alt="Fantasy Predictor text logo"
                         style={{ maxHeight: '60px', objectFit: 'contain' }}
                         onError={(e) => {
                             console.error('Navbar image failed to load');
