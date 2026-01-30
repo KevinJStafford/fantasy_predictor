fed05d78ac267dd38276998b9a1ef2518c08301a
Kevin <staffordkevinjames@gmail.com>
2026-01-30 11:00:41 -0500

    change: colors add: login link to navbar avatar image

--- client/src/components/LandingGrid.js
+++ client/src/components/LandingGrid.js
@@ -1,33 +1,54 @@
-import * as React from 'react'
-import { Button, Box } from '@mui/material'
-import { yellow } from '@mui/material/colors';
+import * as React from 'react';
+import { Button, Box } from '@mui/material';
 import { useHistory } from 'react-router-dom';
+
+const buttonColor = '#ff6c26';
 
 function LandingGrid() {
     const history = useHistory();
-    
+
     return (
-        <>
-        <Box>
-            <h1>Premier League Score Predictor</h1>
-            <h3>Correctly guess the scores of every Premier League match to win points and impress your friends!</h3>
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                minHeight: '60vh',
+                gap: 2,
+                px: 2,
+            }}
+        >
+            <Box sx={{ textAlign: 'center', maxWidth: 600, color: buttonColor }}>
+                <h1>Premier League Score Predictor</h1>
+                <h3>Correctly guess the scores of every Premier League match to win points and impress your friends!</h3>
+            </Box>
+            <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap', justifyContent: 'center' }}>
+                <Button
+                    sx={{
+                        bgcolor: buttonColor,
+                        color: '#f93d3a',
+                        '&:hover': { bgcolor: '#e55a1a', color: '#f93d3a' },
+                    }}
+                    variant="contained"
+                    onClick={() => history.push('/users')}
+                >
+                    Sign Up
+                </Button>
+                <Button
+                    sx={{
+                        color: '#f93d3a',
+                        borderColor: buttonColor,
+                        '&:hover': { borderColor: '#e55a1a', color: '#f93d3a', bgcolor: 'rgba(255, 108, 38, 0.08)' },
+                    }}
+                    variant="outlined"
+                    onClick={() => history.push('/login')}
+                >
+                    Login
+                </Button>
+            </Box>
         </Box>
-        <Button 
-            sx={{bgcolor: yellow[500] }} 
-            variant="contained" 
-            onClick={() => history.push('/users')}
-        >
-            Sign Up
-        </Button>
-        <Button 
-            sx={{bgcolor: yellow[500] }} 
-            variant='outlined' 
-            onClick={() => history.push('/login')}
-        >
-            Login
-        </Button>
-        </>
-    )
+    );
 }
 
 export default LandingGrid;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,19 @@
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
-import { Box, AppBar, Toolbar } from '@mui/material';
+import { Box, AppBar, Toolbar, IconButton } from '@mui/material';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
-import { yellow } from '@mui/material/colors';
+import { useHistory } from 'react-router-dom';
+import { getToken } from '../utils/auth';
 
 function Navbar() {
+    const history = useHistory();
     const logoUrl = (process.env.PUBLIC_URL || '') + '/typeface_logo_new.png';
+    const isLoggedIn = !!getToken();
+
+    const handleAvatarClick = () => {
+        history.push(isLoggedIn ? '/player' : '/login');
+    };
+
     return (
         <AppBar position="static" sx={{ backgroundColor: '#fff', boxShadow: 1 }}>
             <Toolbar sx={{ justifyContent: 'space-between', px: 2 }}>
@@ -22,9 +30,11 @@ function Navbar() {
                     />
                 </Box>
                 <Box>
-                    <Avatar sx={{ bgcolor: yellow[500] }} variant='rounded'>
-                        <AccountBoxIcon />
-                    </Avatar>
+                    <IconButton onClick={handleAvatarClick} aria-label={isLoggedIn ? 'Go to players' : 'Go to login'} sx={{ p: 0 }}>
+                        <Avatar sx={{ bgcolor: '#ff6c26' }} variant="rounded">
+                            <AccountBoxIcon />
+                        </Avatar>
+                    </IconButton>
                 </Box>
             </Toolbar>
         </AppBar>
